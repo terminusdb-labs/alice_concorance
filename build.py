6feb06e6e7f524ff05d80feb151be618d3f717bc
Gavin Mendel-Gleason <gavin@terminusdb.com>
2022-08-23 19:30:05 +0200

    More better naming

--- build.py
+++ build.py
@@ -30,14 +30,14 @@ def add_corpus(client):
     chapter_text = ""
     term_dict = {}
     chapters = []
-    paragraphs = []
-    termdfs = []
+    documents = []
+    termfreqs = []
     with open("corpus/alice_test.txt",) as corpus:
         for line in corpus:
             chapterids = []
             if chapter_end.match(line):
-                paragraphids = []
-                paragraph_count = 0
+                documentids = []
+                document_count = 0
                 if(chapter_count > 0):
                     sentences = nltk.sent_tokenize(chapter_text.lower())
                     for sentence in sentences:
@@ -47,62 +47,63 @@ def add_corpus(client):
                         punctuation_free = [i for i in words if i not in PUNCTUATION]
                         common_word_free = [i for i in punctuation_free if i not in COMMON_WORDS]
                         bgrms = [' '.join(e) for e in nltk.bigrams(common_word_free)]
-                        terms = words + bgrms
+                        terms = words #+ bgrms
                         termids = {}
                         for term in terms:
                             if term not in term_dict:
                                 termid = f".term {term}"
                                 term_dict[term] = {"@type" : "Term",
                                                    "@capture" : termid,
-                                                   "tf" : 1,
+                                                   "df" : 1,
                                                    "term" : term}
                             else:
                                 termobj = term_dict[term]
-                                termobj['tf'] +=1
+                                termobj['df'] +=1
                         counter = collections.Counter(terms)
-                        paragraph_termdfs = []
+                        document_termfreqs = []
                         for term in counter:
-                            df = counter[term]
+                            tf = counter[term]
                             termid = f".term {term}"
-                            termdf = { "@type" : "TermDF",
-                                       "term" : { "@ref" : termid },
-                                       "df" : df }
-                            paragraph_termdfs.append(termdf)
-                            termdfs.append(termdf)
-                        paragraphid = f".paragraph {chapter_count} {paragraph_count}"
-                        paragraphs.append({"@type" : "Paragraph",
-                                           "@capture" : paragraphid,
+                            termfreq = { "@type" : "TermFreq",
+                                         "term" : { "@ref" : termid },
+                                         "tf" : tf }
+                            document_termfreqs.append(termfreq)
+                            termfreqs.append(termfreq)
+                        documentid = f".document {chapter_count} {document_count}"
+                        documents.append({"@type" : "Document",
+                                           "@capture" : documentid,
                                            "text" : sentence,
-                                           "terms" : paragraph_termdfs})
-                        paragraphids.append({"@ref" : paragraphid })
+                                           "terms" : document_termfreqs})
+                        documentids.append({"@ref" : documentid })
 
-                        paragraph_count += 1
+                        document_count += 1
                     chapterid = f".chapter {chapter_count}"
                     chapters.append({"@type" : "Chapter",
                                      "@capture" : chapterid,
                                      "number" : chapter_count,
-                                     "paragraphs" : paragraphids})
+                                     "documents" : documentids})
                     chapterids.append({ "@ref" : chapterid})
                 chapter_count += 1
                 chapter_text = ""
             else:
-                chapter_text += line
+                # We may have newlines that have no space...
+                chapter_text += " " + line
         all_docs = ([{"@type" : "Book",
                       "title" : "Alice in Wonderland",
                       "chapters" : chapterids}]
                     + chapters
-                    + paragraphs
+                    + documents
                     + list(term_dict.values()))
         client.insert_document(all_docs)
 
 def invert_index(client):
     query = Q().group_by(
         ['v:TermDoc'],
-        'v:ParagraphId',
-        'v:ParagraphIds',
+        'v:DocumentId',
+        'v:DocumentIds',
         (Q().triple('v:TermId', 'rdf:type', '@schema:Term') &
-         Q().triple('v:TermdfId','term','v:TermId') &
-         Q().triple('v:ParagraphId', 'terms', 'v:TermdfId') &
+         Q().triple('v:TermfreqId','term','v:TermId') &
+         Q().triple('v:DocumentId', 'terms', 'v:TermfreqId') &
          Q().read_document('v:TermId','v:TermDoc')))
     rows = client.query(query)['bindings']
     termobjs = []
@@ -111,7 +112,7 @@ def invert_index(client):
         term_name = termobj['term']
         print(f"term: {term_name}")
         term_id = termobj['@id']
-        termobj['paragraphs'] = row['ParagraphIds']
+        termobj['documents'] = row['DocumentIds']
         termobjs.append(termobj)
     client.replace_document(termobjs)
 
